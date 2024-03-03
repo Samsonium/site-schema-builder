e43a5680df8ecdae975710139597a935fc3087a9
samsonium <tdesu@vk.com>
2024-03-03 16:54:13 +0300

    Update index file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,14 @@
-// Reexport your entry components here
+import SchemaPage from '$lib/SchemaPage.svelte';
+import BlockRender from '$lib/BlockRender.svelte';
+
+import type { SchemaNode } from '$lib/types/SchemaNode.js';
+import type { SchemaNodeProps } from '$lib/types/SchemaNodeProps.js';
+import type { Size } from '$lib/types/Size.js';
+
+export {
+	SchemaPage,
+	BlockRender,
+	type Size,
+	type SchemaNode,
+	type SchemaNodeProps
+}

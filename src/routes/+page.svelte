48967d7c19d4adfa461f9ada21fe54827c488429
samsonium <tdesu@vk.com>
2024-03-03 16:55:26 +0300

    Move example page schema to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,119 @@
 <script lang="ts">
-	import SchemaPage from '$lib/SchemaPage.svelte';
+	import { onMount } from 'svelte';
+	import { SchemaPage, type SchemaNode } from '$lib';
+	import type { Writable } from 'svelte/store';
+
+	function createBlock(props: Partial<SchemaNode>, ...children: SchemaNode[]): SchemaNode {
+		return {
+			name: 'Some name',
+			description: 'Some description',
+			content: '',
+			children: children ?? [],
+			properties: {},
+			...props,
+		};
+	}
+
+	let blocks: Writable<SchemaNode[]>;
+
+	onMount(() => {
+		blocks.set([
+			createBlock(
+				{
+					properties: {
+						alignItems: 'stretch'
+					}
+				},
+				createBlock({
+					name: 'Logo',
+					properties: {
+						width: {
+							fix: '100px'
+						}
+					}
+				}),
+				createBlock({
+					name: 'Navbar'
+				}),
+				createBlock({
+					name: 'Contacts',
+					properties: {
+						width: {
+							max: '100px'
+						}
+					}
+				})
+			),
+			createBlock({
+				name: 'Menu bar'
+			}),
+			createBlock(
+				{
+					properties: {
+						height: {
+							fix: '300px'
+						}
+					}
+				},
+				createBlock({
+					content: '<',
+					properties: {
+						width: {
+							fix: '64px'
+						},
+						height: {
+							fix: '64px'
+						}
+					}
+				}),
+				createBlock({
+					name: 'Carousel content',
+					properties: {
+						height: {
+							fix: '100%'
+						}
+					}
+				}),
+				createBlock({
+					content: '>',
+					properties: {
+						width: {
+							fix: '64px'
+						},
+						height: {
+							fix: '64px'
+						}
+					}
+				})
+			),
+			createBlock({
+				name: 'Title (popular products)',
+				properties: {
+					height: {
+						fix: '60px'
+					}
+				}
+			}),
+			createBlock(
+				{
+					properties: {
+						wrap: true
+					}
+				},
+				...new Array(10).fill(createBlock({
+					name: 'Card',
+					properties: {
+						width: {
+							fix: `calc(100% / 5)`
+						},
+						height: {
+							fix: '300px'
+						}
+					}
+				}))
+			)
+		]);
+	})
 </script>
 
-<SchemaPage/>
+<SchemaPage bind:blocks/>
